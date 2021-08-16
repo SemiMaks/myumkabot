a1db5f70926b0cdc64fb4b3bb84c58aed9800a3a
Maksim <semimaks@mail.ru>
2021-08-16 19:52:01 +0300

    Добавлен код

--- bot.py
+++ bot.py
@@ -15,13 +15,37 @@ def get_user_info(message):
     markup_inline.add(item_yes, item_no)
     client.send_message(message.chat.id, 'Желаете узнать небольшую информацию о вас?')
 
+@client.callback_query_handler(func=lambda call: True)
+def answer(call):
+    if call.data == 'yes':
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item_id = types.KeyboardButton('МОЙ ID')
+        item_username = types.KeyboardButton('МОЙ НИК')
+
+        markup_reply.add(item_id, item_username)
+        client.send_message(call.message.chat.id, 'Нажмите на одну из кнопок', \
+                            reply_markup=markup_reply
+                            )
+    elif call.data == 'no':
+        pass
+
+
 @client.message_handler(content_types=['text', 'document', 'audio'])
 def get_text(message):
-    if message.text.lower() == 'привет':
-        client.send_message(message.chat.id, 'Привет, неизвестный user!')
-    elif message.text.lower() == '/help':
-        client.send_message(message.chat.id, 'Напиши "привет"')
+    if message.text == 'МОЙ ID':
+        client.send_message(message.chat.id, f'Your ID: {message.from_user.id}')
+    elif message.text == 'МОЙ НИК':
+        client.send_message(message.chat.id, f'Your username: {message.from_user.first_name} {message.from_user.first_name}')
     else:
         client.send_message(message.chat.id, 'Я тебя не понимаю. Напиши /help.')
+
+# @client.message_handler(content_types=['text', 'document', 'audio'])
+# def get_text(message):
+#     if message.text.lower() == 'привет':
+#         client.send_message(message.chat.id, 'Привет, неизвестный user!')
+#     elif message.text.lower() == '/help':
+#         client.send_message(message.chat.id, 'Напиши "привет"')
+#     else:
+#         client.send_message(message.chat.id, 'Я тебя не понимаю. Напиши /help.')
 
 client.polling(none_stop=True, interval=0)

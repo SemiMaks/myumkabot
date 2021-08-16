589ab2becf0f5e4b52e0e835cfaf7e9345ed7fd2
Maksim <semimaks@mail.ru>
2021-08-16 18:25:17 +0300

    Удалил файл с токеном из отслеживаемых

--- bot.py
+++ bot.py
@@ -1,17 +1,27 @@
 import telebot
 import configure
+from telebot import types
 
 client = telebot.TeleBot(configure.config['token'])
 print('Токен принят...')
 
 
+@client.message_handler(commands=['get_info', 'info'])
+def get_user_info(message):
+    markup_inline = types.InlineKeyboardMarkup()
+    item_yes = types.InlineKeyboardButton(text='ДА', callback_data='yes')
+    item_no = types.InlineKeyboardButton(text='НЕТ', callback_data='no')
+
+    markup_inline.add(item_yes, item_no)
+    client.send_message(message.chat.id, 'Желаете узнать небольшую информацию о вас?')
+
 @client.message_handler(content_types=['text', 'document', 'audio'])
 def get_text(message):
     if message.text.lower() == 'привет':
-        client.send_message(message.from_user.id, 'Привет, неизвестный user!')
+        client.send_message(message.chat.id, 'Привет, неизвестный user!')
     elif message.text.lower() == '/help':
-        client.send_message(message.from_user.id, 'Напиши "привет"')
+        client.send_message(message.chat.id, 'Напиши "привет"')
     else:
-        client.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши /help.')
+        client.send_message(message.chat.id, 'Я тебя не понимаю. Напиши /help.')
 
 client.polling(none_stop=True, interval=0)
